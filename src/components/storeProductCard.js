9edfe37fab24ecd19b27df3860a74c0ec2bbffec
Imran Naqvi <imran.naqvi@innovage.io>
2022-01-10 20:39:37 +0500

    images on product

--- src/components/storeProductCard.js
+++ src/components/storeProductCard.js
@@ -37,6 +37,46 @@ const useStyles = makeStyles(theme => ({
     margin: 'auto',
     fontWeight: '500',
   },
+  design: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    height: '100px',
+    width: '100px',
+    [theme.breakpoints.down('sm')]: {
+      height: '70px',
+      width: '70px',
+    },
+  },
+  poster: {
+    height: '190px',
+    width: '190px',
+    borderRadius: '5px',
+    [theme.breakpoints.down('sm')]: {
+      height: '130px',
+      width: '130px',
+    },
+  },
+  phoneCase: {
+    height: '80px',
+    width: '80px',
+    [theme.breakpoints.down('sm')]: {
+      height: '60px',
+      width: '60px',
+      top: '52%',
+    },
+  },
+  mug: {
+    height: '90px',
+    width: '90px',
+    top: '55%',
+    left: '52%',
+    [theme.breakpoints.down('sm')]: {
+      height: '60px',
+      width: '60px',
+    },
+  },  
 }));
 
 const StoreProductCard = ({ product, storeUrl }) => {
@@ -65,6 +105,21 @@ const StoreProductCard = ({ product, storeUrl }) => {
           alt="green iguana"
           className={classes.productImage}
         />
+        {product?.designId && (
+          <img
+            src={product.designId.url}
+            className={[
+              classes.design,
+              product.name === 'Poster'
+                ? classes.poster
+                : product.name === 'Phone Case'
+                ? classes.phoneCase
+                : product.name === 'Mug'
+                ? classes.mug
+                : '',
+            ].join(' ')}
+          />
+        )}
       </Card>
       <Button
         size="medium"
