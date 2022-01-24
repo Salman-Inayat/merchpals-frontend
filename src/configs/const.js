56ef1321623887eeb21ce70ad2688191d3ec799d
Imran Naqvi <imran.naqvi@innovage.io>
2022-01-24 16:55:11 +0500

    escrow flow

--- src/configs/const.js
+++ src/configs/const.js
@@ -1,5 +1,5 @@
 export const baseURL = `${process.env.REACT_APP_SERVER_URL}/api/v1`;
 
 export const calculateProfit = (price, shippingCost, costPrice) => {
-  return (price - shippingCost - costPrice - (0.023 * price + 0.3)) * 0.75;
+  return (price - shippingCost - costPrice - (0.029 * price + 0.3)) * 0.7;
 };
